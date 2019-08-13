f4d1e7e72bcd5713563d76942b22a9dac91bdd92
smhailey <smhailey@msn.com>
2019-08-12 21:49:59 -0600

    Not complete

--- Models/Shack.cs
+++ Models/Shack.cs
@@ -7,7 +7,58 @@ namespace BurgerShack.Models
   {
     public string Location { get; private set; }
     private List<Food> AvailableFood { get; set; }
-    private List<Food> SoldOutFood { get; set; }
+    // private List<Food> SoldOutFood { get; set; }
+
+    public void PrintMenu()
+    {
+      for (int i = 0; i < AvailableFood.Count; i++)
+      {
+        Food f = AvailableFood[i];
+        System.Console.WriteLine($"{i + 1}. {f.Name} Price: {f.Cost}");
+      }
+    }
+
+
+
+    public Food GetFoodFromUserChoice()
+    {
+      if (Int32.TryParse(Console.ReadLine(), out int n))
+      {
+        n = n - 1;
+        if (n < 0 || n >= AvailableFood.Count)
+        {
+          return null;
+        }
+        // add AvailableFood[n] to cart?
+        return AvailableFood[n];
+      }
+      return null;
+    }
+
+
+    public void BuyFood()
+    {
+      var currentlyInTheProcessOfBuyingFood = true;
+      while (currentlyInTheProcessOfBuyingFood)
+      {
+        Console.Clear();
+        PrintMenu();
+        Console.WriteLine("Please make a selection: ");
+        Food food = GetFoodFromUserChoice();
+        if (food == null)
+        {
+          currentlyInTheProcessOfBuyingFood = false;
+        }
+      }
+    }
+
+
+    public void PurchaseTotal()
+    {
+      // display contents of cart
+      // add cost of all items in cart
+      // display total cost of all items in cart
+    }
 
 
 
@@ -21,7 +72,7 @@ namespace BurgerShack.Models
     {
       Location = location;
       AvailableFood = new List<Food>();
-      SoldOutFood = new List<Food>();
+      // SoldOutFood = new List<Food>();
     }
   }
 }
--- Program.cs
+++ Program.cs
@@ -14,7 +14,46 @@ namespace BurgerShack
 
       //NOTE Burgers are implicitly casted to Food
       bs.AddFood(hamburger);
+      bs.AddFood(cheeseburger);
+      bs.AddFood(fries);
 
+      bool quit = false;
+      while (!quit)
+      {
+        Console.Clear();
+        Console.WriteLine("Welcome to Burger Shack");
+        Console.WriteLine("things you can do here");
+        Console.WriteLine("1 - Purchase food");
+        Console.WriteLine("2 - See your total");
+        Console.WriteLine("3 - Quit");
+        Console.WriteLine("What would you like to do?");
+        var userInput = Console.ReadLine();
+        if (Int32.TryParse(userInput, out int choice))
+        {
+          switch (choice)
+          {
+            case 1:
+              Shack.BuyFood();
+              break;
+            case 2:
+              Shack.PurchaseTotal();
+              break;
+            case 3:
+              quit = true;
+              Console.Clear();
+              Console.WriteLine("Goodbye");
+              break;
+            default:
+              System.Console.WriteLine("Invalid Choice Try again");
+              break;
+          }
+        }
+
+
+
+        bs.BuyFood();
+
+      }
     }
   }
 }
